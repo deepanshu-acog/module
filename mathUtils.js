9f3cb205244497979ccd5552b234bf3e33ea4259
deepanshu-acog <deepanshu@aganitha.ai>
2024-02-12 11:26:15 +0530

    mathUtils

---
+++ mathUtils.js
@@ -0,0 +1,7 @@
+const add= (a,b)=>a+b;
+const subtract=(a,b)=>a-b;
+const multiply=(a,b)=>a*b;
+const divide=(a,b)=>a/b;
+const squareRoot=(a)=>Math.sqrt(a);
+module.exports ={add,subtract,multiply,divide,squareRoot};
+
